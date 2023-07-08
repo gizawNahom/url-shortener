e8540082b47859a9d8bb7e9b641ff47adbf45656
Nahom <154278634+gizawNahom@users.noreply.github.com>
2023-07-08 17:59:08 +0300

    refactor: add descriptive display functions

--- packages/client/pages/stat/[id].tsx
+++ packages/client/pages/stat/[id].tsx
@@ -32,74 +32,107 @@ export default function Stat() {
   function displayPage() {
     return (
       <div className="flex flex-col gap-3">
-        <div className="flex flex-row gap-3">
-          {displayLinkIcon()}
-          <p className="text-2xl">{removeProtocol(url?.shortUrl as string)}</p>
-        </div>
-        <p className="text-sm text-gray-500">{url?.longUrl}</p>
-        {hasNoClicks() ? (
-          <div className="flex flex-col justify-center items-center h-96 gap-12">
-            <svg className="h-44 w-44 fill-slate-100" viewBox="0 0 100 100">
-              <rect
-                className="fill-cyan-100"
-                x="12"
-                y="70"
-                width="15"
-                height="20"
-                rx="2"
-                ry="2"
-              />
-              <rect
-                className=" fill-cyan-200"
-                x="32"
-                y="50"
-                width="15"
-                height="40"
-                rx="2"
-                ry="2"
-              />
-              <rect
-                className=" fill-cyan-300"
-                x="52"
-                y="20"
-                width="15"
-                height="70"
-                rx="2"
-                ry="2"
-              />
-              <rect
-                className=" fill-cyan-400"
-                x="72"
-                y="10"
-                width="15"
-                height="80"
-                rx="2"
-                ry="2"
-              />
-              <rect x="0" y="90" width="99" height="4" rx="5" />
-            </svg>
-            <p className="text-2xl">There are no clicks yet</p>
-          </div>
-        ) : (
-          <ClickCount id={router.query.id as string} />
-        )}
+        {displayHeading()}
+        {displayContent()}
       </div>
     );
 
-    function displayLinkIcon() {
-      return <LinkIcon color="cyan-500" />;
+    function displayHeading() {
+      return (
+        <div>
+          <div className="flex flex-row gap-3">
+            {displayLinkIcon()}
+            {displayShortUrl()}
+          </div>
+          {displayLongUrl()}
+        </div>
+      );
+
+      function displayLinkIcon() {
+        return <LinkIcon color="cyan-500" />;
+      }
+
+      function displayShortUrl() {
+        return (
+          <p className="text-2xl">{removeProtocol(url?.shortUrl as string)}</p>
+        );
+      }
+
+      function displayLongUrl() {
+        return <p className="text-sm text-gray-500">{url?.longUrl}</p>;
+      }
+    }
+
+    function displayContent() {
+      return hasNoClicks() ? displayPlaceHolder() : displayClickCount();
+
+      function hasNoClicks() {
+        return url?.totalClicks === 0;
+      }
+
+      function displayPlaceHolder() {
+        return (
+          <div className="flex flex-col justify-center items-center h-96 gap-12">
+            {displayPlaceholderChart()}
+            <p className="text-2xl">There are no clicks yet</p>
+          </div>
+        );
+      }
+
+      function displayPlaceholderChart() {
+        return (
+          <svg className="h-44 w-44 fill-slate-100" viewBox="0 0 100 100">
+            <rect
+              className="fill-cyan-100"
+              x="12"
+              y="70"
+              width="15"
+              height="20"
+              rx="2"
+              ry="2"
+            />
+            <rect
+              className=" fill-cyan-200"
+              x="32"
+              y="50"
+              width="15"
+              height="40"
+              rx="2"
+              ry="2"
+            />
+            <rect
+              className=" fill-cyan-300"
+              x="52"
+              y="20"
+              width="15"
+              height="70"
+              rx="2"
+              ry="2"
+            />
+            <rect
+              className=" fill-cyan-400"
+              x="72"
+              y="10"
+              width="15"
+              height="80"
+              rx="2"
+              ry="2"
+            />
+            <rect x="0" y="90" width="99" height="4" rx="5" />
+          </svg>
+        );
+      }
+
+      function displayClickCount() {
+        return <ClickCount id={router.query.id as string} />;
+      }
     }
   }
 
-  function hasNoClicks() {
-    return url?.totalClicks === 0;
-  }
-
   function displayLoading() {
-    const randInt = Math.floor(Math.random() * (2000 - 1 + 1)) + 1;
     return (
       <div className="flex justify-center items-center h-96">
-        <Loading colored={true} key={randInt} />
+        <Loading colored={true} />
       </div>
     );
   }
