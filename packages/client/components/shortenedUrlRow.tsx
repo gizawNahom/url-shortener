e191e818c6773160f31afc1d7b9f7b7fd14bc361
Nahom <154278634+gizawNahom@users.noreply.github.com>
2023-07-12 18:09:57 +0300

    style: fix stat icon look

--- packages/client/components/shortenedUrlRow.tsx
+++ packages/client/components/shortenedUrlRow.tsx
@@ -44,23 +44,24 @@ export function ShortenedUrlRow({
 
   function displayChartsIcon() {
     return (
-      <div className="h-7 w-8 px-2 py-1 hover:bg-slate-100 rounded-md hover:transition-all fill-slate-400 hover:fill-cyan-500">
-        <Link href={`/stat${getSlugPath()}`}>
-          <svg
-            version="1.0"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 64 64"
-            className="h-full w-full "
-          >
-            <title>Charts Icon</title>
-            <g>
-              <path d="M38,64V4c0-2.211-1.789-4-2-4h-4c-2.211,0-2,1.789-2,4v60H38z" />
-              <path d="M18,64V20c0-2.211-1.789-4-2-4H14c-2.211,0-2,1.789-2,4v44H18z" />
-              <path d="M58,36c0-2.211-1.789-4-2-4h-4c-2.211,0-2,1.789-2,4v28h8V36z" />
-            </g>
-          </svg>
-        </Link>
-      </div>
+      <Link
+        href={`/stat${getSlugPath()}`}
+        className="px-2 py-2 hover:bg-slate-100 rounded-md hover:transition-all fill-black/40 hover:fill-cyan-500"
+      >
+        <svg
+          version="1.0"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 68 68"
+          className="h-4 w-4 "
+        >
+          <title>Charts Icon</title>
+          <g>
+            <path d="M38,64V4c0-2.211-1.789-4-2-4h-4c-2.211,0-2,1.789-2,4v60H38z" />
+            <path d="M18,64V20c0-2.211-1.789-4-2-4H14c-2.211,0-2,1.789-2,4v44H18z" />
+            <path d="M58,36c0-2.211-1.789-4-2-4h-4c-2.211,0-2,1.789-2,4v28h8V36z" />
+          </g>
+        </svg>
+      </Link>
     );
   }
 
