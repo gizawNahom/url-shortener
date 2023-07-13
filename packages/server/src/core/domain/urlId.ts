f09afa4ede1c8f27ae9c8ed11ea10ee6f606f608
Nahom <154278634+gizawNahom@users.noreply.github.com>
2023-07-13 08:05:39 +0300

    refactor: make "urlId" expressive

--- packages/server/src/core/domain/urlId.ts
+++ packages/server/src/core/domain/urlId.ts
@@ -3,13 +3,24 @@ import { ValidationMessages } from '../validationMessages';
 
 export class UrlId {
   constructor(private id: string) {
-    if (!id) throw this.buildValidationError(ValidationMessages.ID_REQUIRED);
-    if (!this.isNineCharacterAlphaNumeric(id))
-      throw this.buildValidationError(ValidationMessages.ID_INVALID);
+    if (this.isFalsy(id)) this.throwIdRequiredError();
+    if (!this.isNineCharacterAlphaNumeric(id)) this.throwIdInvalidError();
+  }
+
+  private isFalsy(id: string) {
+    return !id;
+  }
+
+  private throwIdRequiredError() {
+    throw this.buildValidationError(ValidationMessages.ID_REQUIRED);
   }
 
   private isNineCharacterAlphaNumeric(id: string) {
     return /^[a-zA-Z0-9]{9}$/.test(id);
+  }
+
+  private throwIdInvalidError() {
+    throw this.buildValidationError(ValidationMessages.ID_INVALID);
   }
 
   private buildValidationError(message: string) {
