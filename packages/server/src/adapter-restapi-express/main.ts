b313166a02ebb56f0b056a3af06528e5a7fdacbc
Nahom <154278634+gizawNahom@users.noreply.github.com>
2023-07-21 10:08:13 +0300

    new: make SERVER_DOMAIN required

--- packages/server/src/adapter-restapi-express/main.ts
+++ packages/server/src/adapter-restapi-express/main.ts
@@ -1,5 +1,8 @@
 import app from './app';
 
+if (!process.env.SERVER_DOMAIN)
+  throw new Error('SERVER_DOMAIN env variable is required');
+
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 app.listen(port, () => {
