2cbdded8fe780a4038832124a33befce39e562c8
Nahom <154278634+gizawNahom@users.noreply.github.com>
2023-07-21 07:57:43 +0300

    test: ignore test using cli

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -9,5 +9,4 @@ export default {
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/server',
   transformIgnorePatterns: ['node_modules/(?!(nanoid)/)'],
-  testPathIgnorePatterns: ['providerContract.test', 'mongoUrlStorage.test.ts'],
 };
