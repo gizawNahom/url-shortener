d5cedb73d17ca2305b26b4b45a9be5af404e81b4
Nahom <154278634+gizawNahom@users.noreply.github.com>
2023-07-21 10:41:28 +0300

    new: make CLIENT_DOMAIN required

--- packages/server/src/adapter-restapi-express/main.ts
+++ packages/server/src/adapter-restapi-express/main.ts
@@ -2,6 +2,8 @@ import app from './app';
 
 if (!process.env.SERVER_DOMAIN)
   throw new Error('SERVER_DOMAIN env variable is required');
+if (!process.env.CLIENT_DOMAIN)
+  throw new Error('CLIENT_DOMAIN env variable is required');
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
