a3fc867bd09dfc06c4267e44968cf2296eeedc67
Nahom <154278634+gizawNahom@users.noreply.github.com>
2023-07-22 06:56:16 +0300

    new: set up mongo in main

--- packages/server/src/adapter-restapi-express/main.ts
+++ packages/server/src/adapter-restapi-express/main.ts
@@ -1,12 +1,35 @@
 import app from './app';
+import { setupMongo } from './setup';
 
-if (!process.env.SERVER_DOMAIN)
-  throw new Error('SERVER_DOMAIN env variable is required');
-if (!process.env.CLIENT_ORIGIN)
-  throw new Error('CLIENT_ORIGIN env variable is required');
+class Main {
+  static async start() {
+    try {
+      await tryStart();
+    } catch (error) {
+      console.log('Failure to start: ' + error.message);
+      throw error;
+    }
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+    async function tryStart() {
+      checkRequiredEnvironmentVariables();
+      await setupMongo();
+      startServer();
+    }
 
-app.listen(port, () => {
-  console.log(`[ ready ] PORT: ${port}`);
-});
+    function checkRequiredEnvironmentVariables() {
+      if (!process.env.SERVER_DOMAIN)
+        throw new Error('SERVER_DOMAIN env variable is required');
+      if (!process.env.CLIENT_ORIGIN)
+        throw new Error('CLIENT_ORIGIN env variable is required');
+    }
+
+    function startServer() {
+      const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+      app.listen(port, () => {
+        console.log(`[ ready ] PORT: ${port}`);
+      });
+    }
+  }
+}
+
+Main.start();
---
+++ packages/server/src/adapter-restapi-express/setup.ts
@@ -0,0 +1,38 @@
+import { Db, MongoClient } from 'mongodb';
+import { MongoUrlStorage } from '../mongoUrlStorage';
+import Context from './context';
+
+export async function setupMongo() {
+  const connection = await createConnection();
+  const db = getDB();
+  if (await doesClicksCollectionExist(db))
+    await createClicksTimeSeriesCollection(db);
+  registerStorageInstance();
+
+  async function createConnection() {
+    return await MongoClient.connect(process.env.MONGODB_URI as string);
+  }
+
+  function getDB() {
+    return connection.db(process.env.MONGODB_DATABASE_NAME);
+  }
+
+  async function doesClicksCollectionExist(db: Db) {
+    const collections = await db.listCollections().toArray();
+    return !collections.some((collection) => collection.name === 'clicks');
+  }
+
+  async function createClicksTimeSeriesCollection(db: Db) {
+    await db.createCollection('clicks', {
+      timeseries: {
+        timeField: 'timestamp',
+        metaField: 'urlId',
+        granularity: 'hours',
+      },
+    });
+  }
+
+  function registerStorageInstance() {
+    Context.urlStorage = new MongoUrlStorage(db);
+  }
+}
