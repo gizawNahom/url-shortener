e5d6300a1aeb5a45ce98e7f570d7157b50531d1a
Nahom <154278634+gizawNahom@users.noreply.github.com>
2023-07-24 14:50:27 +0300

    test: add e2e test

--- packages/server-e2e/src/server/server.spec.ts
+++ packages/server-e2e/src/server/server.spec.ts
@@ -1,28 +1,41 @@
-import axios from 'axios';
+import supertest from 'supertest';
 
-describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+let shortUrl: URL;
+const longUrl = 'https://google.com';
 
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ greeting: 'Hello API' });
-  });
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ?? '3000';
+const request = supertest(`http://${host}:${port}`);
+
+test('shortens url', async () => {
+  const res = await request.post('/api/urls').send({ url: longUrl });
+  shortUrl = new URL(res.body.shortUrl);
+
+  expect(res.statusCode).toBe(201);
+  expect(res.body.longUrl).toBe(longUrl);
+  expect(shortUrl.protocol).toBe('https:');
 });
 
-function assertHttps(url: string) {
-  const u = new URL(url);
-  expect(u.protocol).toBe('https:');
-}
+test('redirects url', async () => {
+  const res = await request.get(shortUrl.pathname);
 
-describe('POST /api/urls', () => {
-  it('should return 201 created for a newly shortened url', async () => {
-    const longUrl = 'https://google.com';
+  expect(res.statusCode).toBe(301);
+  expect(res.headers.location).toBe(longUrl);
+});
 
-    const res = await axios.post('/api/urls', { url: longUrl });
+test('saves correct click stat', async () => {
+  const res = await request.get(
+    `/api/urls${shortUrl.pathname}/total-clicks-by-day`
+  );
 
-    expect(res.status).toBe(201);
-    expect(res.data.longUrl).toBe(longUrl);
-    expect(res.data.longUrl).not.toEqual(res.data.shortUrl);
-    assertHttps(res.data.shortUrl);
-  });
+  expect(res.statusCode).toBe(200);
+  expect(res.body.totalClicks).toBe(1);
+  expect(res.body.dailyClickCounts.length).toBe(1);
+  expect(res.body.dailyClickCounts[0].totalClicks).toBe(1);
+  expect(
+    new Date()
+      .toISOString()
+      .slice(0, -4)
+      .includes(res.body.dailyClickCounts[0].day)
+  ).toBe(true);
 });
