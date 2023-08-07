2570263c02cfe7804226994c8e493fd3ffdbc0b4
Nahom <154278634+gizawNahom@users.noreply.github.com>
2023-08-07 12:39:06 +0300

    style: TopDeviceTypes component

--- packages/client/components/topDeviceTypes.tsx
+++ packages/client/components/topDeviceTypes.tsx
@@ -10,7 +10,10 @@ export function TopDeviceTypes({ id }: { id: string }) {
     useState<DeviceTypePercentage[]>();
 
   return (
-    <StatView className="" onFetchData={fetchTopDeviceTypes()}>
+    <StatView
+      className="bg-slate-50 rounded-lg p-5 w-full h-auto"
+      onFetchData={fetchTopDeviceTypes()}
+    >
       {displayTopDeviceTypes()}
     </StatView>
   );
@@ -24,17 +27,29 @@ export function TopDeviceTypes({ id }: { id: string }) {
   function displayTopDeviceTypes() {
     return (
       <div>
-        <p>Top devices</p>
-        <div role="list">
+        <h4 className="text-xs font-bold">Top devices</h4>
+        <div role="list" className="flex flex-col gap-1 pt-2">
           {topDeviceTypes?.map((e, i) => {
             return (
-              <div role="listitem" key={i}>
-                {e.type} {e.percentage}
+              <div role="listitem" key={i} className="text-xs relative">
+                <div className="flex justify-between p-2 z-10 relative">
+                  <p className="font-semibold">{e.type}</p>
+                  <p>{e.percentage}</p>
+                </div>
+                <div
+                  className={`h-full w-[${formatPercentage(
+                    e.percentage
+                  )}] max-w-full bg-cyan-400 absolute top-0 right-0`}
+                ></div>
               </div>
             );
           })}
         </div>
       </div>
     );
+
+    function formatPercentage(percentage: number) {
+      return `${Math.round(percentage * 100)}%`;
+    }
   }
 }
