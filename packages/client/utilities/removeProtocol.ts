4c0cd6dee02ce0e3e071baa8068bc82dc781041a
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-12-12 17:29:24 +0300

    fix: display search params of urls

--- packages/client/utilities/removeProtocol.ts
+++ packages/client/utilities/removeProtocol.ts
@@ -1,6 +1,4 @@
-// export function removeProtocol(url: string | undefined): string {
 export function removeProtocol(url: string): string {
-  //   const u = new URL(url || '');
   const u = new URL(url);
-  return u.hostname + u.pathname;
+  return u.hostname + u.pathname + u.search;
 }
