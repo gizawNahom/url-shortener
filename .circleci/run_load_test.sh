59f6a71eebdcbc54dfde34eb8b7b96f59a5bb08e
Nahom <154278634+gizawNahom@users.noreply.github.com>
2024-12-15 10:52:07 +0300

    build: add load test script

---
+++ .circleci/run_load_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Running load test..."
+npx artillery run ../packages/server/tests/restapi-load/load-test.yml
+EXIT_CODE=$?
+echo $EXIT_CODE
+echo "Load test finished"
+if [ $EXIT_CODE -eq 21 ] || [ $EXIT_CODE -eq 0 ]; then
+  echo "Load test succeeded"
+  exit 0
+else
+  echo "Load test failed"
+  exit $EXIT_CODE
+fi
